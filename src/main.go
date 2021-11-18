e07cdea8ff06d2bbd976fa82897efc70024dca96
HeCorr <hccaseclone@gmail.com>
2021-11-18 03:04:15 -0300

    calculate dynamic padding for printing routes

--- src/main.go
+++ src/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -14,14 +15,27 @@ func main() {
 		fmt.Println("Default bots.yml file not found. Please create it or specify one with -f.")
 		os.Exit(1)
 	}
+
 	botsData, err := readBotsFile(*botsFile)
 	if err != nil {
 		fmt.Printf("Failed to load bots file: %v", err)
 		os.Exit(1)
 	}
-	fmt.Println("Loaded routes:")
+
+	// calculate dynamic paddings based on the longest values
+	var namePd, endpointPd int
 	for _, b := range botsData.Bots {
-		// TODO: calculate padding based on longest value
-		fmt.Printf("  %s: %s -> %s\n", b.Name, b.Endpoint, b.Path)
+		if namePd < len(b.Name) {
+			namePd = len(b.Name)
+		}
+		if endpointPd < len(b.Endpoint) {
+			endpointPd = len(b.Endpoint)
+		}
+	}
+
+	fmt.Println("Loaded routes:")
+
+	for _, b := range botsData.Bots {
+		fmt.Printf("  %"+strconv.Itoa(namePd)+"s: %-"+strconv.Itoa(endpointPd)+"s -> %s\n", b.Name, b.Endpoint, b.Path)
 	}
 }
