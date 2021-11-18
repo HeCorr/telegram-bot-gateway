e33911ebf5d0da1d781a1c9fdf498978623d1c92
HeCorr <hccaseclone@gmail.com>
2021-11-18 02:37:32 -0300

    read bots file and print loaded routes

--- src/main.go
+++ src/main.go
@@ -14,4 +14,14 @@ func main() {
 		fmt.Println("Default bots.yml file not found. Please create it or specify one with -f.")
 		os.Exit(1)
 	}
+	botsData, err := readBotsFile(*botsFile)
+	if err != nil {
+		fmt.Printf("Failed to load bots file: %v", err)
+		os.Exit(1)
+	}
+	fmt.Println("Loaded routes:")
+	for _, b := range botsData.Bots {
+		// TODO: calculate padding based on longest value
+		fmt.Printf("  %s: %s -> %s\n", b.Name, b.Endpoint, b.Path)
+	}
 }
