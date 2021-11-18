fd0ea5b8cf8736d8e4ed449b026dce1b7959cc97
HeCorr <hccaseclone@gmail.com>
2021-11-18 01:33:54 -0300

    implement finding default yml/yaml file
    fileExists() has a (rare) chance of panicking but it doesn't matter much since it (currently) is only called on startup so panicking is okay.

--- src/util.go
+++ src/util.go
@@ -1,10 +1,41 @@
 package main
 
 import (
+	"errors"
 	"os"
 
 	"gopkg.in/yaml.v2"
 )
+
+var (
+	defaultBotsFiles = []string{"bots.yml", "bots.yaml"}
+)
+
+// Checks if file f exists and is not a directory
+// (it panicks if the error is not ErrNotExist)
+func fileExists(f string) bool {
+	info, err := os.Stat(f)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return false
+		} else {
+			panic(err)
+		}
+	}
+	return !info.IsDir()
+}
+
+// Looks for the default bots file (in the current path) in the following order:
+// - bots.yml
+// - bots.yaml
+func findBotsFile() string {
+	for _, f := range defaultBotsFiles {
+		if fileExists(f) {
+			return f
+		}
+	}
+	return ""
+}
 
 // Reads the file f and parses the YAML data into bots
 func readBotsFile(f string) (bots Bots, _ error) {
