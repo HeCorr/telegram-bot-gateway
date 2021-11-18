ee59aad1506df348024f851f53144ee94feb9f4d
HeCorr <hccaseclone@gmail.com>
2021-11-18 02:30:43 -0300

    accept -f flag for specifying bots file

--- src/main.go
+++ src/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
 
 func main() {
-	botsFile := findBotsFile()
-	if botsFile == "" {
+	botsFile := flag.String("f", findBotsFile(), "Use the specified .yaml file")
+	flag.Parse()
+
+	if *botsFile == "" {
 		fmt.Println("No bots.yml file found. Please create one.")
 		os.Exit(1)
 	}
