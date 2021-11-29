7dcce419d422327d4e3bb27addc5d513cfcc2d92
HeCorr <hccaseclone@gmail.com>
2021-11-29 17:53:40 -0300

    define Options struct, add it to Config

--- src/struct.go
+++ src/struct.go
@@ -6,8 +6,16 @@ type Bot struct {
 	Path     string `yaml:"path"`
 }
 
+type Options struct {
+	Key    string `yaml:"key"`
+	Cert   string `yaml:"cert"`
+	Listen string `yaml:"listen"`
+	Strict bool   `yaml:"strict"`
+}
+
 type Config struct {
-	Bots []Bot `yaml:"bots"`
+	Bots    []Bot   `yaml:"bots"`
+	Options Options `yaml:"config"`
 }
 
 // Prefixes Endpoint with a / if necessary
